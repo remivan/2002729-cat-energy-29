7e60e188d1f5a9ea179c6f17d99db9a97d4303c8
Irina <dead.rescuer@yandex.com>
2023-09-17 17:46:42 +0300

    вносит правки с скрипт шапки

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
